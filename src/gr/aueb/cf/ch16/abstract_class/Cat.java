8044cbbf95cc980107944354750bcbd4aea8da18
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-01-20 20:58:42 +0200

    ch-15 -16

---
+++ src/gr/aueb/cf/ch16/abstract_class/Cat.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.ch16.abstract_class;
+
+public class Cat extends Animal {
+    public Cat(){
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return getId() + ", " + getName();
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Niaou");
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println("... ate all the food");
+    }
+}
---
+++ src/gr/aueb/cf/ch16/skeletal2/ISquare.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.ch16.skeletal2;
+
+public interface ISquare {
+    double getDiagonal();
+}
