8bdde6bfe0d8a1dbae7efb8a3fe8cbac93525914
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-02-11 21:00:17 +0200

    reflection

---
+++ src/gr/aueb/cf/ch20/reflection/Teacher.java
@@ -0,0 +1,53 @@
+package gr.aueb.cf.ch20.reflection;
+
+public class Teacher {
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public Teacher() {
+
+    }
+
+    public Teacher(Long id) {
+        this.id = id;
+    }
+
+    public Teacher(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void sayHello () {
+        System.out.println("Hello");
+    }
+
+    public void saySomething(String message) {
+        System.out.println(message);
+    }
+}
