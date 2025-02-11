b022657606ddd5882f2e7fb46202135fa8fab42e
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-02-11 19:04:00 +0200

    regex

---
+++ src/gr/aueb/cf/ch20/ZeroLengthAssertion.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.ch20;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ZeroLengthAssertion {
+
+    public static void main(String[] args) {
+        String s = "BCf";
+
+
+        Pattern pattern = Pattern.compile("^(?=.*?[a-z]).{4,}$");
+        Matcher matcher = pattern.matcher(s);
+
+        while(matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
