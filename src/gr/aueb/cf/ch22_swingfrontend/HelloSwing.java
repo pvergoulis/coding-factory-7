bb5d0ab87c2cc91ac85d7e7ffe7ed7e6daba0862
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-02-19 20:00:13 +0200

    JavaSwing

---
+++ src/gr/aueb/cf/ch22_swingfrontend/HelloSwing.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.ch22_swingfrontend;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloSwing {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+
+
+        jFrame.setTitle("First Swing App");
+        jFrame.setSize(600, 300);
+        jFrame.setLocationRelativeTo(null); // align to center of the screen
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JPanel contentPane = new JPanel();
+        jFrame.setContentPane(contentPane);
+
+        contentPane.add(new JLabel("Hello World"));
+        jFrame.setVisible(true);
+    }
+}
