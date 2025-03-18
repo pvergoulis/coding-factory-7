1ed79382c2549bdba216343093956dcb64218a44
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-03-18 19:31:42 +0200

    IterativeDayTimeServer

---
+++ src/gr/aueb/cf/ch26/IterativeDayTimeServer.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch26;
+
+import org.w3c.dom.CDATASection;
+
+public class IterativeDayTimeServer {
+
+    public static void main(String[] args) {
+        DateTimeServer dateTimeServer = new DateTimeServer();
+        dateTimeServer.start();
+    }
+}
